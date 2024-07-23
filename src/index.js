f1abdecc2f0f53234027c1495862ac5cc3c1b836
Harshit Bamotra <harshitbamotra.01@gmail.com>
2024-07-24 01:46:19 +0530

    FEAT: added basic express server setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const {PORT} = require("./config/server.config");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+
+app.get('/ping', (req, res)=>{
+    return res.json({
+        msg: "Problem servive is up"
+    });
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port: ${PORT}`);
+})
