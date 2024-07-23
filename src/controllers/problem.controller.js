20a690ab288292243eb39a7f1526716d298cfcc0
Harshit Bamotra <harshitbamotra.01@gmail.com>
2024-07-24 02:25:42 +0530

    FEAT: added routing structure

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,35 @@
+
+function pingProblemController(req, res){
+    return res.json({
+        msg: "api is working fine"
+    });
+}
+
+function addProblem(req, res){
+
+}
+
+function getProblem(req, res){
+    
+}
+
+function getProblems(req, res){
+    
+}
+
+function deleteProblem(req, res){
+
+}
+
+function updateProblem(req, res){
+
+}
+
+module.exports = {
+    addProblem,
+    getProblem,
+    getProblems, 
+    deleteProblem,
+    updateProblem,
+    pingProblemController
+};
